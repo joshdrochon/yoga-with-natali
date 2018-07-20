c0d2be49db2885beeb18f0c70e551fd2f44b6efd
Josh Rochon <joshdrochon@gmail.com>
2018-07-20 15:36:36 -0700

    resolve deployment issues

--- src/components/home/HomeView1.js
+++ src/components/home/HomeView1.js
@@ -41,8 +41,8 @@ const HomeView1 = () => (
     </style>
     <div style={styles.flexChild}>
       <h2 className="shadow">Inspiration of the day...</h2>
-      <h3 className="shadow quote">
-        Happiness is not something ready made. It comes from your own actions.
+      <h3 className="shadow">
+        "Happiness is not something ready made. It comes from your own actions."
       </h3>
     </div>
     <hr style={styles.hr} />
